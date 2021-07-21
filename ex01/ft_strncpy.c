5bc8ca20a9a4a14397094aabe5eae62bf944e142
sdxsd <willmaguire@icloud.com>
2021-07-21 15:09:04 +0200

    Fixes for MOULINETTE

--- ex01/ft_strncpy.c
+++ ex01/ft_strncpy.c
@@ -1,3 +1,5 @@
+#include <stdio.h>
+
 char	*ft_strncpy(char *dest, char *src, unsigned int n)
 {
 	unsigned int	iterator;
@@ -13,6 +15,20 @@ char	*ft_strncpy(char *dest, char *src, unsigned int n)
 			++iterator;
 		}
 		dest[iterator] = '\0';
+		if (iterator < n)
+			while(iterator != n)
+			{
+				dest[iterator] = '\0';
+				iterator++;
+			}
 		return (dest);
 	}
+}
+
+int main()
+{
+	char *src = "Wat Leuk!";
+	char dest[] = "Niet leuk!";
+	ft_strncpy(&dest[0], src, 9);
+	printf("%s\n", dest);
 }
--- ex02/ft_str_is_alpha.c
+++ ex02/ft_str_is_alpha.c
@@ -1,3 +1,5 @@
+#include <stdio.h>
+
 int	ft_str_is_alpha(char *str)
 {
 	int	iterator;
@@ -5,17 +7,27 @@ int	ft_str_is_alpha(char *str)
 	iterator = 0;
 	while (str[iterator] != '\0')
 	{
-		if (str[iterator] > 'z' || str[iterator] < 'a' )
-		{
-			if (str[iterator] > 'Z' || str[iterator] < 'A')
+		if ((str[iterator] > 'z' || str[iterator] < 'a') && (str[iterator] > 'Z' || str[iterator] < 'A'))
 				return (0);
-			else
-				return (1);
-		}
 		else
 			++iterator;
 	}
 	if (iterator == 0)
 		return (1);
 	return (1);
+}
+
+int main()
+{
+	char *str = "GOED";
+	char *str2 = "1234";
+	char *str3 = "in3";
+	char *str4 = "DSSD#$SF#343qFD";
+
+	int x = ft_str_is_alpha(str);
+	int y = ft_str_is_alpha(str2);
+	int c = ft_str_is_alpha(str3);
+	int b = ft_str_is_alpha(str4);
+
+	printf("x: %d | y: %d | c: %d | c: %d\n", x, y, c, b);
 }
