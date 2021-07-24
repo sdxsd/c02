c23e916248412605df801078b1064ca07dabd045
sdxsd <willmaguire@icloud.com>
2021-07-24 16:04:59 +0200

    Norminette

--- ex01/ft_strncpy.c
+++ ex01/ft_strncpy.c
@@ -16,19 +16,13 @@ char	*ft_strncpy(char *dest, char *src, unsigned int n)
 		}
 		dest[iterator] = '\0';
 		if (iterator < n)
-			while(iterator != n)
+		{
+			while (iterator != n)
 			{
 				dest[iterator] = '\0';
 				iterator++;
 			}
+		}
 		return (dest);
 	}
-}
-
-int main()
-{
-	char *src = "Wat Leuk!";
-	char dest[] = "Niet leuk!";
-	ft_strncpy(&dest[0], src, 9);
-	printf("%s\n", dest);
 }
--- ex02/ft_str_is_alpha.c
+++ ex02/ft_str_is_alpha.c
@@ -7,27 +7,13 @@ int	ft_str_is_alpha(char *str)
 	iterator = 0;
 	while (str[iterator] != '\0')
 	{
-		if ((str[iterator] > 'z' || str[iterator] < 'a') && (str[iterator] > 'Z' || str[iterator] < 'A'))
-				return (0);
+		if ((str[iterator] > 'z' || str[iterator] < 'a') && \
+		 (str[iterator] > 'Z' || str[iterator] < 'A'))
+			return (0);
 		else
 			++iterator;
 	}
 	if (iterator == 0)
 		return (1);
 	return (1);
-}
-
-int main()
-{
-	char *str = "GOED";
-	char *str2 = "1234";
-	char *str3 = "in3";
-	char *str4 = "DSSD#$SF#343qFD";
-
-	int x = ft_str_is_alpha(str);
-	int y = ft_str_is_alpha(str2);
-	int c = ft_str_is_alpha(str3);
-	int b = ft_str_is_alpha(str4);
-
-	printf("x: %d | y: %d | c: %d | c: %d\n", x, y, c, b);
 }
